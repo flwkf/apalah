097913aca91e90d122d136b9b6ce4d6cf65740c3
Rizal Rahman Rizkika <94729829+flwkf@users.noreply.github.com>
2024-12-06 17:39:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from pymongo import MongoClient
+from pymongo.errors import ConnectionError
 
 # Ambil Mongo URI dari secrets
 def get_database():
@@ -7,7 +8,13 @@ def get_database():
     client = MongoClient(CONNECTION_STRING)
     return client['coba']
 
-db = get_database()
-collection = db['cobaa']
-data = collection.find_one()
-st.write(data)
+try:
+    db = get_database()
+    collection = db['cobaa']
+    data = collection.find_one()
+    if data:
+        st.write(data)
+    else:
+        st.write("Tidak ada data yang ditemukan.")
+except ConnectionError:
+    st.error("Tidak dapat terhubung ke database. Periksa URI dan koneksi internet Anda.")
