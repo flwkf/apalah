3c1edbafa9c09637e9790385f9e8ce783d798f3c
Rizal Rahman Rizkika <94729829+flwkf@users.noreply.github.com>
2024-12-06 17:59:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from pymongo import MongoClient
-from pymongo.errors import ServerSelectionTimeoutError
 
 # Ambil Mongo URI dari secrets
 def get_database():
@@ -8,13 +7,7 @@ def get_database():
     client = MongoClient(CONNECTION_STRING)
     return client['coba']
 
-try:
-    db = get_database()
-    collection = db['cobaa']
-    data = collection.find_one()
-    if data:
-        st.write(data)
-    else:
-        st.write("Tidak ada data yang ditemukan.")
-except ServerSelectionTimeoutError:
-    st.error("Tidak dapat terhubung ke database. Periksa URI dan koneksi internet Anda.")
+db = get_database()
+collection = db['cobaa']
+data = collection.find_one()
+st.write(data)
