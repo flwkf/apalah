b9740cfc135a230c573e22a6aaeee2b314483ce6
Rizal Rahman Rizkika <94729829+flwkf@users.noreply.github.com>
2024-12-06 16:58:20 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from pymongo import MongoClient
+
+# Ambil Mongo URI dari secrets
+def get_database():
+    CONNECTION_STRING = st.secrets["MONGO_URI"]
+    client = MongoClient(CONNECTION_STRING)
+    return client['coba']
+
+db = get_database()
+st.write(db)
